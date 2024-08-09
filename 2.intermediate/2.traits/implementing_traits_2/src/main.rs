8f41d65e7ea3b184ae43c8f606152ba434af5781
Guillaume Fortin <gfortin@neetrof.fr>
2024-08-09 14:24:20 +0200

    complete intermediate traits exercices

---
+++ 2.intermediate/2.traits/implementing_traits_2/src/main.rs
@@ -0,0 +1,26 @@
+// Complete the code by addressing the TODO.
+
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+// TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut result = self;
+        result.push(String::from("Bar"));
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_vec_pop_eq_bar() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+}
