6887d9c07f506992c79e041bc9c710b87b2f16c5
Guillaume Fortin <gfortin@neetrof.fr>
2024-08-21 13:58:50 +0200

    complete intermediate exercices -> Function Pointers

---
+++ 2.intermediate/5.functional_features_in_rust/3.function_pointers/coercing_from_closures/src/main.rs
@@ -0,0 +1,15 @@
+// Fix the code by addressing the TODO.
+
+fn invoker<T>(logic: fn(T), arg: T) {
+    logic(arg);
+}
+
+fn main() {
+    // TODO: shift below declaration to somewhere else
+    //let greeting = String::from("Nice to meet you");
+    let greet = |name| {
+        let greeting = String::from("Nice to meet you");
+        println!("{greeting} {name}!");
+    };
+    invoker(greet, "Jenny");
+}
